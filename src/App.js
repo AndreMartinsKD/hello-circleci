13ed3f071377ddd4ef01259fed2bc9e5b47ee133
André Martins <andre.martins@koerber.digital>
2020-05-08 16:42:56 +0100

    Hello Circle Ci

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Körber CircleCi
+          Hello Circle CI
         </p>
         <a
           className="App-link"
